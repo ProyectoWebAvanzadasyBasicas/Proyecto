dca5c684a4acb81d5b8acab7b828f9c7f3f680f9
Jordan Cortes <jc111518@ingenieria.sanmartin.edu.co>
2014-10-02 19:02:57 -0500

    repo update

--- interfaz_grafica/js/validacionIngresos.js
+++ interfaz_grafica/js/validacionIngresos.js
@@ -3,12 +3,12 @@ function valida_envia(validaEntero){
   validaEntero= parseInt(numeros) 
   document.valida.numeros.value=numeros 
   if (/([?1234567890][.][1234567890][1234567890])+$/.test(numeros)){
-   alert("El valor solo numero  vuelve intentar el campo texto");
+   alert("El valor recibe solo valor numerico, vuelva insertar el valor requerido");
    document.valida.numeros.focus() 
    return 0;    
  }else{ 
    if (numeros<50000){
-   alert(" no puede meter menos de 50 mil pesos")
+   alert("No puede ingresar menos de 50 mil pesos")
     document.valida.numeros.focus() 
     return 0; 
   }
@@ -20,7 +20,7 @@ if (numeros==""){
  return 0; 
 } 
 if (numeros>50000){
- alert(" gracias esta guardado base de datos"); 
+ alert("Gracias se ha guardado en la  base de datos"); 
 document.valida.submit(); 
 }
 }
